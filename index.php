2e7c5e36de316b86767641ac11577ef90b816098
Gauarv-Jain <jaing438@gmail.com>
2023-01-27 18:55:46 +0530

    index

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    require_once 'includes/header.php'
+?>
+
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+    <?php 
+        echo "Nothing to see haere. just a home page now move on"
+    ?>
+
+<?php
+    require_once 'includes/footer.php'
+?>
+
